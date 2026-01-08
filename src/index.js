f1c776977b0c6b5af92081e5e1e0e0b366c92f32
Shiv Bawa <bawashiv120406@gmail.com>
2026-01-08 23:46:16 +0530

    conect to mongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// require()`dotenv .config({path: './env})
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+/*
+import mongoose from mongoodse;
+import {DB_NAME} from "./constants";
+*/
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.group(`App is listening on port ${process.env.PORT}`);
+        })
+
+    }catch(error){
+        console.error("ERROR: ", error)
+    throw err
+    }
+})()
+
+*/
