dcfff679667c94b63e4c99e693d9fda2bd71a3ca
Shiv Bawa <bawashiv120406@gmail.com>
2026-01-21 21:25:35 +0530

    aii setup config

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin : process.env,CORS_ORIGIN,
+    origin : process.env.CORS_ORIGIN,
     credentials: true
 }))
 
@@ -14,6 +14,15 @@ app.use(express.urlencoded({extented: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// routes import 
+
+import userRouter from './routes/user.routes.js'
+
+// routes declaration 
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
 
 export {app}
 
